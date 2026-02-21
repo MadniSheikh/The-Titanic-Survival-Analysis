50622aadd085fbadbad445629bb4fa9ca4c11124
MadniSheikh <smmadni127147@gmail.com>
2026-02-21 16:11:58 +0530

    Feature Transformation done with modification in pipeline helpers

--- Notebook/pipelineHelpers.py
+++ Notebook/pipelineHelpers.py
@@ -2,73 +2,98 @@ import pandas as pd
 import numpy as np
 
 def extractTitle(X):
+
+    if isinstance(X, pd.DataFrame):
+        X = X.iloc[:, 0]
     
     NameTitle = X.apply(lambda x: x.split(",")[1].split()[0])
 
     result_df = pd.DataFrame(
         {
             'NameTitle': NameTitle
-        }
+        }, index=X.index                            ##I did this because there were inconsistences in row indices that lead to error
     )
 
     return result_df
 
-def ageCategory(x):                                                             ##RED ONE HAS BEEN PICKED
-    if x<15:
+def ageCategory2(x):                                                 #Green
+    if x<7:
         return "Childrens"
-    elif x<35:
-        return "Adults"
+    elif x<20:
+        return "teen"
+    elif x<40:
+        return "Peak"
     elif x<60:
-        return "Middle"
+        return "Senior"
     else:
         return "old"
     
 
 def extractAgeCategory(X):
+
+    if isinstance(X, pd.DataFrame):
+        X = X.iloc[:, 0]
     
-    AgeCategory = X.apply(ageCategory)
+    AgeCategory = X.apply(ageCategory2)
 
     result_df = pd.DataFrame(
         {
             'AgeCategory': AgeCategory
-        }
+        }, index=X.index
     )
 
     return result_df
+
+def fam_category(s):                                        ##Green
+    if s == 0:
+        return "Alone"
+    elif 0<s<4:
+        return "Small_fam"
+    else:
+        return "Big_fam"
 
 def famCategory(X):
     
     FamSize = X.iloc[:, 0] + X.iloc[:, 1]
 
+    FamSize = FamSize.apply(fam_category)
+
     result_df = pd.DataFrame(
         {
             'FamSize': FamSize
-        }
+        }, index=X.index
     )
 
     return result_df
 
 def ticketCategory(X):
+
+    if isinstance(X, pd.DataFrame):
+        X = X.iloc[:, 0]
+
     TicketCat = X.apply(lambda s: s.split()[0])
     TicketCat = np.where(TicketCat.str.isdigit(), "NA", TicketCat) 
 
     result_df = pd.DataFrame(
         {
             'TicketCat': TicketCat
-        }
+        }, index=X.index
     )
 
     return result_df 
 
 def cabinCategory(X):
+
+    if isinstance(X, pd.DataFrame):
+        X = X.iloc[:, 0]
     
     CabinCategory = X.str[0]
-    CabinCategory = np.where(CabinCategory.str.isdigit(), "MISSING", CabinCategory)
+    CabinCategory = np.where(CabinCategory.str.isdigit(), "NA", CabinCategory)
 
     result_df = pd.DataFrame(
         {
             'CabinCategory': CabinCategory
-        }
+        }, index=X.index
     )
 
     return result_df 
